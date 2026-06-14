d96c52f09f9a70afec9e9761cc4b77fd7093b8ae
RohitMunjal25 <mobilemunjal@gmail.com>
2026-06-14 12:28:46 +0530

    force ipv4 gmail smtp

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,17 +1,17 @@
 const nodemailer = require("nodemailer");
 
-const transporter =
-nodemailer.createTransport({
-  service: "gmail",
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4, // IPv4 force
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
-const sendOTPEmail =
-async (email, otp) => {
-
+const sendOTPEmail = async (email, otp) => {
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
     to: email,
@@ -20,9 +20,8 @@ async (email, otp) => {
       <h2>Perfume Store Verification</h2>
       <h1>${otp}</h1>
       <p>OTP valid for 5 minutes.</p>
-    `
+    `,
   });
-
 };
 
 module.exports = sendOTPEmail;
